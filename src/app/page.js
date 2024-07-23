7d7a9ca6ee004f92f1ab6780d7549a77be382ed3
gabriel <gabrieldecarvalhu@gmail.com>
2024-07-23 15:31:05 -0300

    usegsap e melhores praticas react

--- src/app/page.js
+++ src/app/page.js
@@ -4,79 +4,87 @@ import './page.css'
 import img1 from '../../public/assets/img1.jpg'
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
-import { gsap } from "gsap";
-import {React} from 'react'
+import  gsap  from "gsap";
+import { React } from 'react'
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(useGSAP);
 
 
 export default function Home() {
   const [itens, setItems] = useState();
   const img_item = useRef([])
+  const [imageCenter, setImageCenter] = useState(img1)
+
   useEffect(() => {
     const generatedItems = [];
-    
-    
-    //console.log(previewImage)
 
     for (let i = 0; i < 150; i++) {
       const caminho = "./assets/img" + ((i % 15) + 1) + ".jpg";
-      //console.log(caminho)
-     
+
 
       generatedItems.push(
-        <div className='item' key={i} ref={img_item}>
+        <div className='item' key={i} ref={(el) => (img_item.current[i] = el)}>
           <img src={caminho} alt='' />
-
         </div>
       )
-      
+
     }
     setItems(generatedItems);
 
 
-   
 
-  },[])
 
-  useEffect(() => {
-    const previewImage = document.querySelector(".center_img img");
-    const items = document.querySelectorAll(".item");
-    console.log(items)
-    const numberOfItems = items.length;
+  }, [])
+
+  useGSAP(() => {
+    const numberOfItems = img_item.current.length;
     const angleIncrement = 360 / numberOfItems;
 
-    items.forEach((item, index) => {
+    img_item.current.forEach((item, index) => {
       gsap.set(item, {
         rotateY: 90,
         rotateZ: index * angleIncrement - 90,
         transformOrigin: "50% 400px",
       })
 
-      item.addEventListener("mouseover" , function () {
+      const handleMouseOver = () => {
         const imgInsideItem = item.querySelector("img");
-        previewImage.src = imgInsideItem.src;
+        setImageCenter(imgInsideItem.src);
 
         gsap.to(item, {
-          x:5,
-          y:10,
-          z:20,
-          ease:"power2.out",
-          duration:0.5
-        });
-        item.addEventListener("mouseout", function () {
-          gsap.to(item, {
-            x: 0,
-            y: 0,
-            z: 0,
-            ease: "power2.out",
-            durtion: 0.5,
-          });
+          x: 5,
+          y: 10,
+          z: 20,
+          ease: "power2.out",
+          duration: 0.5
         });
 
-      })
+      }
+
+      const handleMouseOut = () => {
+        gsap.to(item, {
+          x: 0,
+          y: 0,
+          z: 0,
+          ease: "power2.out",
+          durtion: 0.5,
+        });
+
+      }
+
+      item.addEventListener("mouseover", handleMouseOver);
+      item.addEventListener("mouseout", handleMouseOut);
+
+      return () => {
+        item.removeEventListener("mouseover", handleMouseOver);
+        item.removeEventListener("mouseout", handleMouseOut);
+      };
+
 
     })
 
-  },[itens])
+  }, [itens])
   return (
     <>
       <nav className='top'>
@@ -94,7 +102,7 @@ export default function Home() {
 
       <section>
         <div className='center_img'>
-          <img src={img1} alt=""/>
+          <img src={imageCenter} alt="" />
         </div>
         <div className='container'>
           <div className='gallery'>
